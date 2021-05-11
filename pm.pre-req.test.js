e25c8ba3af53ae78d8943acdb65af073bfd3b7d3
R. Fadhil <ridhanfadhilah@gmail.com>
2021-05-11 12:15:26 +0700

    pre-request script

--- pm.pre-req.test.js
+++ pm.pre-req.test.js
@@ -3,8 +3,9 @@ value++
 pm.collectionVariables.set("var_increment", value);
 
 const uuid = require('uuid')
-let email=uuid()+'@domain.com'
+let email = uuid()+'@domain.com'
 pm.globals.set('email',email)
 
 const moment = require('moment');
-pm.globals.set("Now", moment().format());
+pm.globals.set("currentTime", moment().format("YYYY-MM-DDTHH:mm:ss+07:00"));
+pm.globals.set("currentTimePlusAnHour", moment().add(1, "hours").format("YYYY-MM-DDTHH:mm:ss+07:00"));
